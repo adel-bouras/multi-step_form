4960679e18bfcd852cdbb12a5dc798af9da156cd
adel-bouras <eyyad.kareem1212@gmail.com>
2025-01-26 19:14:09 +0100

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ import Card from "@/components/card/Card";
 export default function Home() {
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
-      <h1 className="motion-safe:animate-bounce select-none before:absolute ds:before:w-[50px] ds:before:h-[50px] before:bg-red-500 ds:before:rounded-xl ds:before:-top-[50px] ds:before:-left-[50px]  rounded-xl relative font-poppins ds:text-9xl font-bold drop-shadow-lg text-red-700 xs:text-7xl xs:before:w-[30px] xs:before:h-[30px] xs:before:-top-[30px] xs:before:-left-[30px] xs:before:rounded-md">
-        Adel
-      </h1>
+      <Card />
+      <div className="ds:hidden xs:block">
+        <Sidebar />
+      </div>
     </div>
   );
 }
