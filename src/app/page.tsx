19062e108247f21c0bfd90ee10cf9889ec9db667
Bouras Adel <136173110+adel-bouras@users.noreply.github.com>
2025-01-28 01:20:13 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Sidebar from "@/components/sidebar/Sidebar";
 import Card from "@/components/card/Card";
 
-export default function Home() {
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
       <Card />
